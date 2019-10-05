024cbf2bad8a9ef98ce8bf8599040c63d92657c5
Ben Crane <bencrane5.12@gmail.com>
2019-10-05 12:24:21 -0400

    Point before term-kit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const { printScreen } = require('./lib/screen');
+const { getPlayers } = require('./lib/input');
+
+const game = {};
+async function main() {
+    printScreen();
+    game.players = await getPlayers();
+    console.log(game.players);
+}
+
+console.log("Players entered");
