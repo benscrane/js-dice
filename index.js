4137b2f691df3402631769f32cc1a4e98d716dc4
Ben Crane <bencrane5.12@gmail.com>
2019-10-05 15:41:27 -0400

    Add game helper

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ function startGame() {
         data.game.lives.push(3);
     }
 }
-screen.drawScreen(data);
 
 async function main() {
+    screen.drawScreen(data);
     let input;
     do {
         input = await screen.getPlayers(data);
         if (input.trim().length > 0) {
             data.game.players.push(input.trim());
-            screen.drawScreen(data);
+            screen.updatePlayers(data.game);
         }
     } while (input.trim().length > 0)
     startGame();
