b9d4905779ed34176a855aef716e1c166cfd0618
Ben Crane <bencrane5.12@gmail.com>
2019-10-05 13:09:57 -0400

    Add lives

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const { printScreen } = require('./lib/screen');
-const { getPlayers } = require('./lib/input');
+const screen = require('./lib/screen');
 
-const game = {};
-async function main() {
-    printScreen();
-    game.players = await getPlayers();
-    console.log(game.players);
+data = {
+    game: {
+        players: [
+            'Ben',
+            'Ennio',
+            'Seamus'
+        ]
+    }
 }
 
-console.log("Players entered");
+function startGame() {
+    data.game.lives = [];
+    for (let i = 0; i < data.game.players.length; i++) {
+        data.game.lives.push(3);
+    }
+}
+startGame();
+screen.drawScreen(data);
