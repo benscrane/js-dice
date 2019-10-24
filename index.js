288592410e4267fdf4c1320e92e0b4aa06d8bee8
Ben Crane <bencrane5.12@gmail.com>
2019-10-24 13:11:32 -0400

    Updates

--- index.js
+++ index.js
@@ -9,8 +9,73 @@ data = {
         firstRoll: 0,
         numRolls: 3,
         rolls: [],
+        lives: [],
         highest: null,
-        lowest: null
+        lowest: null,
+        previousLoser: 0
+    }
+}
+
+function resetRound() {
+    data.game.numRolls = 3;
+    data.game.rolls = game.seedRolls(data.game.players);
+    data.highest = null;
+    data.lowest = null;
+}
+
+async function runRound() {
+    // for each player alive, run turn
+    let startIndex = data.game.previousLoser || 0;
+    resetRound();
+    for (let i = 0; i < data.game.players.length; i++) {
+        let index = (i + startIndex) % data.game.players.length;
+        if (data.game.lives[index] > 0) {
+            data.game.turn = index;
+            screen.drawScreen(data);
+            screen.updateCurrentTurn(data.game);
+            await runTurn();
+        }
+    }
+    endRound();
+    screen.drawScreen(data);
+}
+
+function endRound() {
+    // find the loser, no ties yet
+    data.game.highest = game.getHighestRoll(data.game.rolls);
+    data.game.lowest = game.getLowestRoll(data.game.rolls);
+    let loser = data.game.rolls.indexOf(data.game.lowest);
+    let livesLost = data.game.highest == 21 ? 2 : 1;
+    data.game.lives[loser]-= livesLost;
+    if (data.game.lives[loser] < 0) {
+        data.game.lives[loser] = 0;
+    }
+    // set next turn
+    data.game.previousLoser = loser;
+}
+
+async function runTurn() {
+    // let player roll
+    let roll = 0;
+    // roll automatically
+    let rolledDice = game.rollDice();
+    screen.updateRollDisplay(data.game, rolledDice, roll);
+    roll++;
+    while (roll < data.game.numRolls) {
+        let rollResult = await screen.getRollDecision();
+        if (rollResult === false) {
+            break;
+        }
+        rolledDice = game.rollDice();
+        screen.updateRollDisplay(data.game, rolledDice, roll);
+        roll++;
+    }
+    // rolled dice is the final roll
+    screen.clearRollDisplay();
+    data.game.rolls[data.game.turn] = rolledDice;
+    // if player is first, set limit on number of rolls
+    if (data.game.previousLoser = data.game.turn) {
+        data.game.numRolls = roll;
     }
 }
 
@@ -28,21 +93,10 @@ async function main() {
     data.game.rolls = game.seedRolls(data.game.players);
     screen.drawScreen(data);
     // game loop
-    while (!data.game.finished) {
-        screen.drawScreen(data);
-        screen.updateCurrentTurn(data.game);
-        let roll = 0;
-        while (roll < data.game.numRolls) {
-            let rollResult = await screen.getRollDecision();
-            if (rollResult === false) {
-                break;
-            }
-            // roll the dice, 
-            let rolledDice = game.rollDice();
-            screen.updateRollDisplay(data.game, rolledDice, roll);
-            roll++;
-        }
-        data.game.finished = true;
+    while (!data.game.finished && data.game.players.length > 0) {
+        // run round
+        await runRound();
+        data.game.finished = game.isGameOver(data.game);
     }
     screen.clearRollDisplay();
     screen.endGame();
