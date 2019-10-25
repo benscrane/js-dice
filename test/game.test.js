bf418016a47893322f2399168282268995c93b81
Ben Crane <bencrane5.12@gmail.com>
2019-10-25 15:22:47 -0400

    Update coverage

--- test/game.test.js
+++ test/game.test.js
@@ -6,7 +6,7 @@ const assert = require('chai').assert;
 describe('Game', function() {
     describe('#seedLives()', function() {
         it('should return array of correct length', function() {
-            assert.equal(seedLives(["a", "b", "c"]).length, 3);
+            assert.lengthOf(seedLives(["a", "b", "c"]), 3);
         })
     })
 })
